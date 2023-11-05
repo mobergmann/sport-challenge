d9d625420c2866f67d896bc57b520581af52ad68
mobergmann <mobergmann@pm.me>
2023-11-06 00:10:51 +0100

    adding a home log

--- public/home.js
+++ public/home.js
@@ -8,12 +8,34 @@ Date.prototype.getWeek = function() {
     return Math.ceil(dayOfYear / 7)
 };
 
-let activities_per_user = new Map();
-{
+Array.prototype.sum = function() {
+    let sum = 0;
+    for (const element of this) {
+        sum += element;
+    }
+    return sum;
+}
+
+Array.prototype.sum = function(key) {
+    let sum = 0;
+    for (const element of this) {
+        sum += element[key];
+    }
+    return sum;
+}
+
+/// download all activities and store them in a map assigning each user a list of the associated activities
+/// the map is ordered by the *sum of the amount of the activities* of each user
+async function prepare_activities_data() {
+    let activities_per_user = new Map();
+
     for (const activity of await get_all_activities()) {
-        if (! activities_per_user.has(activity.author_id)) {
+        // if author not alreay in map, insert the author as a key and an emptpy list
+        if (!activities_per_user.has(activity.author_id)) {
             activities_per_user.set(activity.author_id, []);
         }
+
+        // add the activity to the author
         activities_per_user.get(activity.author_id).push({
             id: activity.id,
             author_id: activity.author_id,
@@ -24,7 +46,7 @@ let activities_per_user = new Map();
         });
     }
 
-    // sort activities of each user by date
+    // for each user sort it's activities by date
     for (const [_, activities] of activities_per_user) {
         activities.sort((lhs, rhs) => {
             // negative if a is less than b,
@@ -39,16 +61,43 @@ let activities_per_user = new Map();
             }
         });
     }
+
+    // sort the activities_per_user map so that the person with the most amounts is inserted first
+    let tmp = [];
+    for (const [author_id, activities] of activities_per_user) {
+        tmp.push({author_id: author_id, activities: activities});
+    }
+    tmp = tmp.sort((lhs, rhs) => {
+        let lhs_sum = lhs.activities.reduce((a, b) => a.amount + b.amount, 0);
+        let rhs_sum = rhs.activities.reduce((a, b) => a.amount + b.amount, 0);
+
+        // negative if a is less than b,
+        // positive if a is greater than b,
+        // zero if they are equal.
+        if (lhs_sum < rhs_sum) {
+            return -1;
+        } else if (lhs_sum > rhs_sum) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+    // convert placeholder to map
+    let sorted_activities_per_user = new Map();
+    for (const i of tmp) {
+        sorted_activities_per_user.set(i.author_id, i.activities);
+    }
+
+    return sorted_activities_per_user;
 }
 
-console.debug(activities_per_user);
-
-// display the chart
-{
+/// display the activities in a chart
+function init_chart(activities_per_user) {
+    // display the chart
     const x_axis_labels = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    
+
     // prepare for each user the y-Axis
-    let push_up_per_day = [];
+    let activities_per_day = [];
     for (const [author_id, activities] of activities_per_user) {
         // push ups done at day's index
         let amounts = [0, 0, 0, 0, 0, 0, 0];
@@ -57,13 +106,13 @@ console.debug(activities_per_user);
             if (activities[i].start_time.getWeek() !== new Date().getWeek()) {
                 continue;
             }
-            
+
             // sum the activity's amount to its corresponding weekday
             const activity_weekday = activities[i].start_time.getDay();
             amounts[activity_weekday] += activities[i].amount;
         }
-        
-        push_up_per_day.push({
+
+        activities_per_day.push({
             // todo: replace with author name
             label: author_id,
             data: amounts,
@@ -77,7 +126,7 @@ console.debug(activities_per_user);
     // currently y_axis stores the amount done at each day.
     // we instead  want to store the sum of the activity.amount from 0 to i
     let y_axis = [];
-    for (let dataset of push_up_per_day) {
+    for (let dataset of activities_per_day) {
         for (let i = 1; i < dataset.data.length; ++i) {
             dataset.data[i] = dataset.data[i] + dataset.data[i-1];
         }
@@ -107,3 +156,49 @@ console.debug(activities_per_user);
         }
     });
 }
+
+function init_log(activities_per_user) {
+    // test to see if the browser supports the HTML template element by checking
+    // for the presence of the template element's content attribute.
+    if (!("content" in document.createElement("template"))) {
+        alert("Your browser doesn't support templates. We cannot display the site properly. Try updating it or using a more up to date browser.");
+    }
+
+    const parent = document.querySelector("#ref-log-list");
+    const template = document.querySelector("#template-log");
+
+    let place = 1;
+    for (const [author_id, activities] of activities_per_user) {
+        const clone = template.content.cloneNode(true);
+        // add the data to the log entry
+        clone.querySelector("#template-log-place").innerHTML = place;
+        clone.querySelector("#template-log-username").innerHTML = author_id;
+        clone.querySelector("#template-log-amount").innerHTML = activities.sum("amount"); // todo: issue
+                                                                 // (a, b) => a.amount + b.amount,
+        // add the activities to the log entry
+        let activities_list = clone.querySelector("#template-log-activities");
+        let activity_template = document.querySelector("#template-log-activity");
+        for (const activity of activities) {
+            const clone = activity_template.content.cloneNode(true);
+            clone.querySelector("#template-log-activity-start-time").value = activity.start_time.toISOString().slice(0,16);
+            clone.querySelector("#template-log-activity-duration").value = activity.end_time - activity.start_time;
+            clone.querySelector("#template-log-activity-amount").value = activity.amount;
+
+            activities_list.appendChild(clone);
+        }
+
+        parent.appendChild(clone);
+        ++place;
+    }
+}
+
+
+
+async function main() {
+    let data = await prepare_activities_data();
+
+    init_chart(data);
+    init_log(data);
+}
+
+await main();
