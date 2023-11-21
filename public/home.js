6cdc67f60e53a9224ce45abc1ea22aa41294b3b0
rmirau <38428148+rmirau@users.noreply.github.com>
2023-11-21 22:29:52 +0100

    Update public/home.js
    Co-authored-by: Moritz Bergmann <mobergmann@pm.me>

--- public/home.js
+++ public/home.js
@@ -191,7 +191,7 @@ function init_chart(activities_per_user, user_by_id) {
             return null;
         }
 
-        if(getCookie('uid') == author_id) {
+        if (getCookie('uid') === author_id) {
             color = 'Red';
         } else {
             color = 'rgb(75, 192, 192)';
