d35ac1f5451ff183b478af7f0d1aab6be169aea4
rmirau <38428148+rmirau@users.noreply.github.com>
2023-11-21 22:30:21 +0100

    Update public/scripts/helper.js
    Co-authored-by: Moritz Bergmann <mobergmann@pm.me>

--- public/scripts/helper.js
+++ public/scripts/helper.js
@@ -2,7 +2,7 @@
 export function setCookie(name,value,days) {
     let expires = "";
     if (days) {
-        var date = new Date();
+        let date = new Date();
         date.setTime(date.getTime() + (days*24*60*60*1000));
         expires = "; expires=" + date.toUTCString();
     }
