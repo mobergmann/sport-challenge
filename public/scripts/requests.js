5a6496f17ea16cf925b77bef5f640e857421bbf6
mobergmann <mobergmann@pm.me>
2023-11-27 22:17:41 +0100

    frontend

--- public/scripts/requests.js
+++ public/scripts/requests.js
@@ -5,26 +5,13 @@ export const BASE_AUTH_URL = `${BASE_URL}/auth`;
 export const BASE_ACTIVITIES_URL = `${BASE_URL}/activities`;
 
 
-export async function do_request(request, body_expected = true) {
-    return await fetch(request)
-        .then((response) => {
-            if (response.status === 200) {
-                if (!body_expected) {
-                    return response;
-                } else {
-                    return response.json();
-                }
-            } else {
-                throw new Error("Something went wrong on API server!");
-            }
-        })
-        .then((response) => {
-            return response;
-        })
-        .catch((error) => {
-            console.error(error);
-            throw error;
-        });
+export async function do_request(request, handler = (res) => {}, error_handler = (error) => {}) {
+    try {
+        const response = await fetch(request);
+        handler(response);
+    } catch (error) {
+        error_handler(error);
+    }
 }
 
 //#region auth
